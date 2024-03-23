d985a3f478d3a57f6039bc4931a6d3aa3b8482c9
Debjyoti0404 <debsen2003@gmail.com>
2024-03-23 20:35:57 +0530

    change regarding mobile view

--- network/forms.py
+++ network/forms.py
@@ -4,5 +4,6 @@ class PostForm(forms.Form):
     post_content = forms.CharField(label="", widget=forms.Textarea(attrs={
         "rows":"5",
         "placeholder":"What's on your mind?",
+        "class": "form-control",
         "style":"width: 100%;font-size:24px;border:none;",
         }), max_length=1000)
